c6bb0e423d1b9972a940b01120791bc84ceb3111
Bjarne Riis <bjarne@briis.com>
2022-08-25 08:18:35 +0200

    Bump to version 1.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="iqua_softener",
-    version="1.0.2",
+    version="1.0.3",
     license="MIT",
     author="Artur Zabroński",
     author_email="artur.zabronski@gmail.com",
